37fbe50acc0b0adae332fe0cd3332aebbf50f52b
elumarketing <marketing@elightup.com>
2023-12-29 16:23:36 +0700

    change read

--- main.js
+++ main.js
@@ -24,10 +24,10 @@ let data = [];
 add.addEventListener("click", (e) => {
       e.preventDefault();
       // check if exist localstoreage
-
+      update =false
       check = localStorage.getItem('data')
       // console.log(check);
-      if (check == null){
+      if (check == null && update){
             data.push({
                   masv: masv.value,
                   hoten: hoten.value,
@@ -51,9 +51,10 @@ add.addEventListener("click", (e) => {
                   phone: phone.value,
             }
             check2.push(data1)
-
             localStorage.setItem("data", JSON.stringify(check2));
+            location.reload();
       }
+
 
       // console.log(data);
       // console.log(masv.value);
@@ -67,23 +68,62 @@ add.addEventListener("click", (e) => {
 var tasks = document.querySelector("#dssv tbody");
 var dssvData = JSON.parse(localStorage.getItem('data'))
 
-dssvData.map(function(value,index){
-      return (tasks.innerHTML += `
-      <tr>
-            <td scope="row">${++index}</td>
-            <td>${value.masv}</td>
-            <td>${value.hoten}</td>
-            <td>${value.cmnd}</td>
-            <td>${value.email}</td>
-            <td>${value.phone}</td>
-      </tr>
-      `
+function getdata(dssvData){
+      dssvData.map(function(value,index){
+            // console.log(index);
+            // ind = index++;
+            return (tasks.innerHTML += `
+            <tr>
+                  <td scope="row"></td>
+                  <td>${value.masv}</td>
+                  <td>${value.hoten}</td>
+                  <td>${value.cmnd}</td>
+                  <td>${value.email}</td>
+                  <td>${value.phone}</td>
+                  <td onclick="editsv(${index})">Edit</td>
+                  <td onclick="delsv(${index})">Delete</td>
+            </tr>
+            `
 
-      )
+            )
 
-})
+      })
 
+}
+getdata(dssvData);
+function  delsv(id){
+      /*
+      Tìm trong local vị trí index và lấy ra
+      sau đó xóa trong local rồi cập nhập lại
+      */
+      getsv = JSON.parse(localStorage.getItem('data'));
 
+      getsv.splice(getsv[id],1)
+      localStorage.setItem("data", JSON.stringify(getsv));
+      // location.reload();
+      getdata(getsv);
+      location.reload();
+      console.log(getsv);
+}
+
+function editsv(id, data){
+      // khi chon data se fill vao form va
+      getsv = JSON.parse(localStorage.getItem('data'));
+      masv.value = getsv[id].masv
+       hoten.value =  getsv[id].hoten
+      cmnd.value =  getsv[id].cmnd
+       email.value =  getsv[id].email
+       phone.value =  getsv[id].phone
+       upd ={
+            masv: masv.value,
+            hoten: hoten.value,
+            cmnd: cmnd.value,
+            email: email.value,
+            phone: phone.value,
+      }
+       updatesv(id,upd)
+}
 // console.log(dssvData);
 
 // console.log(hoten);
+
